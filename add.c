53cc8e75d701d0ab53f9783e459304a4001175f5
Janota1 <janetjanota@gmail.com>
2023-06-25 13:06:54 -0700

    add.c

--- add.c
+++ add.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- *@f - adds the top two elements of the stack.
+ *@f_add - adds the top two elements of the stack.
  * @head: stack head
  * @counter: line_number
  * Return: no return
